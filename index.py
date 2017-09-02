8a6e03d5b733772e1fc7127692b785ad743deb89
acalcagni <annalisa.calcagni@gmail.com>
2017-09-02 16:06:40 +0200

    Ingenti modifiche

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
+from pprint import pprint
+import pdfkit
+
 # render_template -> indica il file html da visualizzare
 # redirect -> la funzione di questo foglio python da eseguire
 import os
+import re
 from pymongo import MongoClient
+
 
 def connect():
 # Substitute the 5 pieces of information you got when creating
@@ -17,15 +22,53 @@ app = Flask(__name__)
 handle = connect()
 
 # Bind our index page to both www.domain.com/ and www.domain.com/index
-@app.route("/index" ,methods=['GET'])
+@app.route("/index", methods=['GET'])
 @app.route("/", methods=['GET'])
 def index():
-    quesiti = [x for x in handle.tasks.find()]
-    return render_template('index.html', quesiti=quesiti)
+    return render_template('index.html')
 
-@app.route("/find", methods=['GET'])
-def find():
-    return render_template('cercauno.html')
+@app.route("/indicazioninazionali", methods=['GET'])
+def indicazioninazionali():
+    traguardi = handle.indicazioni.find({'traguardo':{'$exists': True}})
+    return render_template('indicazioninazionali.html', traguardi=traguardi)
+
+@app.route("/elencoquesiti", methods=['GET'])
+def elencoquesiti():
+    result = request.args
+    quesiti = handle.tasks.find().sort([("titolo",1)])
+    return render_template('elencoquesiti.html', quesiti=quesiti)
+
+@app.route("/dettaglioquesito")
+def dettaglioquesito():
+    return render_template('dettaglioquesito.html', task=findTask(), testo=True)
+
+@app.route("/soluzione")
+def soluzione():
+     return render_template('soluzione.html', task=findTask(), soluzione=True)
+
+@app.route("/argomento_informatico")
+def argomento_informatico():
+     return render_template('argomento_informatico.html', task=findTask(), argomento=True)
+
+@app.route("/generaPdf", methods=['POST'])
+def generaPdf():
+    data = request.form
+    task = handle.tasks.find_one({'id':data.get("id_quesito")})
+    rendered = render_template('pdftemplate.html', task = task)
+    pdf = pdfkit.from_string(rendered, False)
+
+    response= make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+
+    return response
+
+
+def findTask():
+    result = request.args["id_quesito"]
+    task = handle.tasks.find_one({'id':result})
+    return task
+
 
 # Remove the "debug=True" for production
 if __name__ == '__main__':
@@ -33,3 +76,24 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
 
     app.run(host='0.0.0.0', port=port, debug=True)
+
+
+# Ricerca delle info sul task, ora rimpiazzato dallo script dataTables
+#@app.route("/search", methods=['GET'])
+#def search():
+#    result = request.args
+#    titolo = result["titolo"]
+#    anno = result["anno"]
+#    categoria = result["categoria"]
+#    regx = re.compile(titolo, re.IGNORECASE)
+#    annox = re.compile(anno)
+#    categoriax = re.compile(categoria)
+#    quesiti = handle.tasks.find({'titolo':{'$regex':regx},'anno':annox, 'categoria':categoriax}).sort([("titolo",1)])
+#    return render_template('index.html', quesiti=quesiti)
+
+
+#@app.route("/write", methods=['POST'])
+#def write():
+#    userinput = request.form.get("userinput")
+#    risultato= handle.tasks.find({'titolo' : userinput})
+#    return render_template ("cercauno.html", ris=risultato)
---
+++ static/script.js
@@ -0,0 +1,123 @@
+
+$(document).ready(function(){
+
+
+ $("#myModal").draggable({
+      handle: ".modal-header"
+  });
+
+
+    $('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
+
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+        }
+    });
+
+var table = $('#myTable').DataTable({
+  "language":{
+      "lengthMenu":"Visualizza _MENU_ elementi per pagina",
+      "search":         "Search:",
+      "zeroRecords":    "No matching records found",
+      "paginate": {
+          "first":      "First",
+          "last":       "Last",
+          "next":       "Successivo",
+          "previous":   "Precedente"
+        },
+      },
+      initComplete: function () {
+            this.api().columns(1).every( function () {
+                var column = this;
+                var select = $('#annoSearch')
+                
+                select.append( '<option value=""></option>' )
+                    
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+
+           this.api().columns(2).every( function () {
+                var column = this;
+                var select = $('#categoriaSearch')
+                select.append( '<option value=""></option>' )
+                  
+                    select.append( '<option value="KiloBebras (8-10anni)">KiloBebras (8-10anni)</option>' )
+                    select.append( '<option value="MegaBebras (10-12anni)">MegaBebras (10-12anni)</option>' )
+                    select.append( '<option value="GigaBebras (12-13anni)">GigaBebras (12-13anni)</option>' )
+                    select.append( '<option value="TeraBebras (13-15anni)">TeraBebras (13-15anni)</option>' )
+                    select.append( '<option value="PetaBebras (15-18anni)">PetaBebras (15-18anni)</option>' )
+                
+                  .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                          
+                        );
+ 
+                        column
+                            .search( val ? ''+val+'' : '', true, false )
+                            .draw();
+                    } );
+ 
+                
+            } );
+
+        },
+});
+
+
+
+$('#titoloSearch').on( 'keyup', function () {
+    table
+        .columns( 0 )
+        .search( this.value )
+        .draw();
+} );
+
+
+});
